d819103a6fd42ca40c37712e22d7840fc679294c
EA Rashel <rashel320@gmail.com>
2017-11-11 19:47:01 +0600

    Initial Commit

---
+++ Side Menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Side Menu
+//
+//  Created by Estique on 11/11/17.
+//  Copyright © 2017 Estique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
